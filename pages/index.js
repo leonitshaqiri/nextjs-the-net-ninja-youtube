e45e04fd8b6132c664001b86fa635019b5849c5d
leonitshaqiri <65737961+leonitshaqiri@users.noreply.github.com>
2022-04-24 13:28:13 +0200

    #5 - Creating a Layout Component

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,13 @@
-import Footer from "../comps/Footer";
-import Navbar from "../comps/Navbar";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <h1>Homepage</h1>
       <p>
         Cillum commodo aliquip voluptate dolor. Ad labore aliqua reprehenderit
-        ullamco laboris aliquip. In consectetur culpa velit consectetur amet
+        ullamco laboris aliquip. In conseactetur culpa velit consectetur amet
         irure esse. Eiusmod consequat sunt consectetur pariatur minim proident
         velit elit. Irure ex laborum dolor fugiat consequat tempor mollit.
       </p>
@@ -23,7 +20,6 @@ export default function Home() {
       <Link href="/ninjas">
         <a>See Ninja Listening</a>
       </Link>
-      <Footer />
     </div>
   );
 }
