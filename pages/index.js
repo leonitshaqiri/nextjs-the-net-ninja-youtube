94df983e43eec6fcf768c174ac9684fc7cbd807e
leonitshaqiri <65737961+leonitshaqiri@users.noreply.github.com>
2022-04-24 20:35:07 +0200

    #9 - Images & Metadata

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,32 @@
+import Head from "next/head";
 import Link from "next/link";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>Homepage</h1>
-      <p className={styles.text}>
-        Cillum commodo aliquip voluptate dolor. Ad labore aliqua reprehenderit
-        ullamco laboris aliquip. In conseactetur culpa velit consectetur amet
-        irure esse. Eiusmod consequat sunt consectetur pariatur minim proident
-        velit elit. Irure ex laborum dolor fugiat consequat tempor mollit.
-      </p>
-      <p className={styles.text}>
-        Cillum commodo aliquip voluptate dolor. Ad labore aliqua reprehenderit
-        ullamco laboris aliquip. In consectetur culpa velit consectetur amet
-        irure esse. Eiusmod consequat sunt consectetur pariatur minim proident
-        velit elit. Irure ex laborum dolor fugiat consequat tempor mollit.
-      </p>
-      <Link href="/ninjas">
-        <a className={styles.btn}>See Ninja Listening</a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1 className={styles.title}>Homepage</h1>
+        <p className={styles.text}>
+          Cillum commodo aliquip voluptate dolor. Ad labore aliqua reprehenderit
+          ullamco laboris aliquip. In conseactetur culpa velit consectetur amet
+          irure esse. Eiusmod consequat sunt consectetur pariatur minim proident
+          velit elit. Irure ex laborum dolor fugiat consequat tempor mollit.
+        </p>
+        <p className={styles.text}>
+          Cillum commodo aliquip voluptate dolor. Ad labore aliqua reprehenderit
+          ullamco laboris aliquip. In consectetur culpa velit consectetur amet
+          irure esse. Eiusmod consequat sunt consectetur pariatur minim proident
+          velit elit. Irure ex laborum dolor fugiat consequat tempor mollit.
+        </p>
+        <Link href="/ninjas">
+          <a className={styles.btn}>See Ninja Listening</a>
+        </Link>
+      </div>
+    </>
   );
 }
